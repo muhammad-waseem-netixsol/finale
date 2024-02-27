712e6325bf0367fb94a5c974331e57125ca25348
muhammad-waseem-netixsol <151015897+muhammad-waseem-netixsol@users.noreply.github.com>
2024-02-27 12:18:09 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
+16
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-   // Define CORS options
-   const corsOptions: CorsOptions = {
-    origin: ['https://finale-frontend.vercel.app'], 
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // Define CORS options
+  // Define CORS options
+  const corsOptions: CorsOptions = {
+    origin: ['https://finale-frontend.vercel.app'],
     methods:  ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
   };
   // Enable CORS with options
   app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+  await app.listen(3002);
 }
 bootstrap();
